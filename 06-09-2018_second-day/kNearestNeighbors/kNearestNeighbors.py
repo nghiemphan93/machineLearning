6c71acb49623ead17d86ce3632f09e3dceb699d1
Nghiem Phan <nghiemphan93@gmail.com>
2018-09-07 15:41:15 +0200

    refactor kNearestNeighbor

--- 06-09-2018_second-day/kNearestNeighbors/kNearestNeighbors.py
+++ 06-09-2018_second-day/kNearestNeighbors/kNearestNeighbors.py
@@ -1,6 +1,4 @@
 import operator
-
-
 from numpy import tile, array, append
 import numpy as np
 from sklearn import datasets
@@ -12,37 +10,48 @@ from sklearn.model_selection import train_test_split
 def createDataSet():
     group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
+
     return group, labels
 
 def classify(testData, dataSet, labels, k):
     size = dataSet.shape[0]
+    distance = calcDistance(dataSet, size, testData)
+    sortedDistanceIndex = distance.argsort()
+    sortedLabelCount = voteLabel(k, labels, sortedDistanceIndex)
 
-    diffMatrix = np.tile(testData, (size, 1)) - dataSet      # [point - point, point - point...]
-    squareDiffMatrix = diffMatrix**2
+    return sortedLabelCount
 
-    squareDistances = squareDiffMatrix.sum(axis=1)      # [x^2 + y^2, x^2 + y^2...]
-    distances = squareDistances**0.5
+def calcDistance(dataSet, size, testData):
+    diffMatrix = np.tile(testData, (size, 1)) - dataSet
+    squareDiffMatrix = diffMatrix ** 2
+    squareDistances = squareDiffMatrix.sum(axis=1)
+    distances = np.sqrt(squareDistances)
 
+    return distances
 
-    sortedDistIndicies = distances.argsort()            # sort distances, save just index []
-    labelCount={}                                       #dictionary (label: counter)
-
+def voteLabel(k, labels, sortedDistIndicies):
+    labelCount = {}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]      # get label given index
-        labelCount[voteIlabel] = labelCount.get(voteIlabel,0) + 1   # increase counter according to label
+        voteIlabel = labels[sortedDistIndicies[i]]
+        labelCount[voteIlabel] = labelCount.get(voteIlabel, 0) + 1
 
-    # sort the dictionary (label: counter) by counter reverse
-    # save to array
     sortedClassCount = sorted(labelCount.items(), key=operator.itemgetter(1), reverse=True)
 
-    # return most voted label
     return sortedClassCount
+
+def calcSuccess(result, dataTestLabel):
+    counter = 0
+    for i in range(len(result)):
+        if (dataTestLabel[i] == result[i]):
+            counter = counter + 1
+            
+    return counter / len(result) * 100.0
 # endregion
 
 
 # First example
-# Label schould be B with 2 votes
 data, label = createDataSet()           # dummy data
+# Test Label should be B with 2 votes
 print(classify([0,0], data, label, 3))
 
 
@@ -56,7 +65,7 @@ label = iris.target
 labelIndex = iris.target_names
 
 
-# Take 80 data set for  training
+# 80 data set for  training
 # 20 data set for testing
 dataTrain, dataTest, dataTrainLabel, dataTestLabel = train_test_split(data, label, test_size=20)
 
@@ -68,27 +77,19 @@ print("data test", dataTest)
 print("test label", dataTestLabel)
 print()
 
-
-
 # Run the nKK algorithm
-result = array([])
+result = np.array([])
 for i in range(len(dataTest)):
     temp = classify(dataTest[i], dataTrain, dataTrainLabel, 6)
     result = np.append(result, temp[0][0])
 
-
-
-# Calculate probability of success
-counter = 0
-for i in range (len(result)):
-    if(dataTestLabel[i] == result[i]):
-        counter = counter + 1
-success = counter / len(result) * 100.0
+# Calculate Success
+success = calcSuccess(result, dataTestLabel)
 
 # Print results
 print("Calculated ", result)
 print("Truth      ", dataTestLabel)
-print("Succesful: ", success, " %")
+print("Successful: ", success, " %")
 
 
 
